86414b36d5a849f7e1e975f8c3e48d107832b1e0
Ian Young <ian@iangreenleaf.com>
2020-08-14 00:14:10 -0500

    More tags for T&M tables

--- _plugins/summary_table.rb
+++ _plugins/summary_table.rb
@@ -2,11 +2,12 @@ module Jekyll
   class SummaryTableTagBlock < Liquid::Block
     def initialize(tag_name, markup, tokens)
       super
-      @type = markup
+      @type, @title = markup.split(", ")
     end
 
     def render(context)
-      "<table class='summary #{@type}'>#{super}</table>"
+      @caption = unless @title.nil? then "<caption class='title'><h3>#{@title}</h3></caption>" end
+      "<table class='summary #{@type}'>#{@caption}#{super}</table>"
     end
   end
 
@@ -17,10 +18,34 @@ module Jekyll
     end
 
     def render(context)
-      "<tr><td class='description'>#{@description}</td><td class='amount'>#{@amount}</td></tr>"
+      "<tr class='line-item'><td class='description'>#{@description}</td><td class='amount'>#{@amount}</td></tr>"
+    end
+  end
+
+  class SummaryTableSubtotalHeading < Liquid::Tag
+    def initialize(tag_name, text, tokens)
+      super
+      @title = text
+    end
+
+    def render(context)
+      "<tr class='subtotal-heading'><th colspan='2'>#{@title}</th></tr>"
+    end
+  end
+
+  class SummaryTableTotal < Liquid::Tag
+    def initialize(tag_name, text, tokens)
+      super
+      @amount = text
+    end
+
+    def render(context)
+      "<tr class='total'><td class='description'>Total</td><td class='amount'>#{@amount}</td></tr>"
     end
   end
 end
 
 Liquid::Template.register_tag('summary', Jekyll::SummaryTableTagBlock)
 Liquid::Template.register_tag('line_item', Jekyll::SummaryTableLineItem)
+Liquid::Template.register_tag('subtotal_heading', Jekyll::SummaryTableSubtotalHeading)
+Liquid::Template.register_tag('total', Jekyll::SummaryTableTotal)
