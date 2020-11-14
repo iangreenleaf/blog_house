3c82187a90e47cb9d117874db3bad1b362ef4456
Ian Young <ian@iangreenleaf.com>
2020-11-13 23:02:05 -0600

    Summary table subtotals

--- _plugins/summary_table.rb
+++ _plugins/summary_table.rb
@@ -43,9 +43,21 @@ module Jekyll
       "<tr class='total'><td class='description'>Total</td><td class='amount'>#{@amount}</td></tr>"
     end
   end
+
+  class SummaryTableSubtotal < Liquid::Tag
+    def initialize(tag_name, text, tokens)
+      super
+      @amount = text
+    end
+
+    def render(context)
+      "<tr class='subtotal'><td class='description'></td><td class='amount'>#{@amount}</td></tr>"
+    end
+  end
 end
 
 Liquid::Template.register_tag('summary', Jekyll::SummaryTableTagBlock)
 Liquid::Template.register_tag('line_item', Jekyll::SummaryTableLineItem)
 Liquid::Template.register_tag('subtotal_heading', Jekyll::SummaryTableSubtotalHeading)
 Liquid::Template.register_tag('total', Jekyll::SummaryTableTotal)
+Liquid::Template.register_tag('subtotal', Jekyll::SummaryTableSubtotal)
