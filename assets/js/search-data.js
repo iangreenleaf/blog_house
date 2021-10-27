a5b779a7ed7cf689e45acd9367ff58c7481b8677
Ian Young <ian@iangreenleaf.com>
2021-10-26 20:36:38 -0500

    Automated commit from build

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -2,65 +2,70 @@ var store = [{
         "title": "Our Old House",
         "excerpt":"In April 2017, we became the proud owners of a beautiful old house in Minneapolis. The way I see it, we didn’t so much find the house as the house found us. In February 2017, we were starting to think about looking at houses, but we were not yet looking...","categories": [],
         "tags": [],
-        "url": "https://house.youngram.com/2018/10/04/our-old-house.html"
+        "url": "https://house.youngram.net/2018/10/04/our-old-house.html"
       },{
         "title": "What to expect from this blog",
         "excerpt":"This is as much my personal house journal as it is a public blog.I’ll try to keep it interesting, but if you’re expecting a lot of polish, you’re probably in the wrong place.In order for this thing to work, I need to keep posting to it.The only way that’s going...","categories": [],
         "tags": ["Meta"],
-        "url": "https://house.youngram.com/2018/12/13/what-to-expect.html"
+        "url": "https://house.youngram.net/2018/12/13/what-to-expect.html"
       },{
         "title": "The Garage",
         "excerpt":"We’ll start this blog with the first true project we tackled after moving in: the garage.This was an obvious place for us to start because it felt like a very doable entry point, and because it was pretty urgent. Why? Let’s review. The garage is… not in great shape.It’s as...","categories": [],
         "tags": [],
-        "url": "https://house.youngram.com/2019/06/20/the-garage.html"
+        "url": "https://house.youngram.net/2019/06/20/the-garage.html"
       },{
         "title": "The Chimney",
         "excerpt":"A recurring theme of this journal is going to be a comment our inspector made concerning the previous owners of our house: “they had a lot of start in them, but not a lot of finish.” One of the many partially-completed projects we inherited when we bought the house was...","categories": [],
         "tags": ["Exterior","Contractors","Not a lot of finish"],
-        "url": "https://house.youngram.com/the-chimney.html"
+        "url": "https://house.youngram.net/the-chimney.html"
       },{
         "title": "Basement Egress",
         "excerpt":"The basement was mostly finished when we bought the house.It still needs drywall and ceiling and a few other finishing touches, but it was already waterproofed, insulated, and had a new cement floor with in-floor hydronic heat.Time will tell if we actually take advantage of it being finished or continue...","categories": [],
         "tags": ["Basement","Exterior","Contractors","Not a lot of finish"],
-        "url": "https://house.youngram.com/basement-egress.html"
+        "url": "https://house.youngram.net/basement-egress.html"
       },{
         "title": "The first-year garden",
         "excerpt":"I had maintained a small garden at both of the last two places we rented, so now that we had a place of our own I was excited to get a garden going.However, we closed on the house at the end of April, and were consumed with moving and some...","categories": [],
         "tags": ["Garden"],
-        "url": "https://house.youngram.com/the-first-year-garden.html"
+        "url": "https://house.youngram.net/the-first-year-garden.html"
       },{
         "title": "The Mud Head",
         "excerpt":"A bolt of inspiration The basement egress window installation left us with a giant pile of dirt. I slowly redistributed this to other places around the yard as I added garden beds, but some of the the subsoil they had dug up was heavy clay.Hard to work within garden beds,...","categories": [],
         "tags": ["Garden"],
-        "url": "https://house.youngram.com/the-mud-head.html"
+        "url": "https://house.youngram.net/the-mud-head.html"
       },{
         "title": "How to plan a garden, part one: the vision",
         "excerpt":"With our new house, I was very excited to start planning our garden and landscaping, but to be honest also very intimidated.I had done a bit of gardening at our rentals over the years, but always at a fairly small scale.I knew I wanted to go much bigger with my...","categories": [],
         "tags": ["Garden","How to"],
-        "url": "https://house.youngram.com/how-to-plan-a-garden-part-one.html"
+        "url": "https://house.youngram.net/how-to-plan-a-garden-part-one.html"
       },{
         "title": "How to plan a garden, part two: the details",
         "excerpt":"My journey thus far in landscape design had arrived at a high-level plan for the yard that split it into segments with themes driving the design. This was an excellent starting point for the design, but I still had to fill in a lot of details and figure out what...","categories": [],
         "tags": ["Garden","How to"],
-        "url": "https://house.youngram.com/how-to-plan-a-garden-part-two.html"
+        "url": "https://house.youngram.net/how-to-plan-a-garden-part-two.html"
       },{
         "title": "The Rain Garden",
         "excerpt":"The rain garden is one of my proudest landscaping achievements and probably the most unqualified success I’ve had.I want to tell you all about it, but if you’re just here for the pretty pictures, I understand.You can skip all the words and get straight to the good stuff. Why a...","categories": [],
         "tags": ["Garden"],
-        "url": "https://house.youngram.com/the-rain-garden.html"
+        "url": "https://house.youngram.net/the-rain-garden.html"
       },{
         "title": "(H.V.)AC",
         "excerpt":"The other half-finished big-ticket project that came with the house (besides the chimney) was central air conditioning.The house had been fitted with supplies and a return for high-velocity air conditioning, and lines had been run for a condenser and air handler, but no units had been installed. High-velocity AC is...","categories": [],
         "tags": ["Boring","Contractors","Not a lot of finish"],
-        "url": "https://house.youngram.com/air-conditioning.html"
+        "url": "https://house.youngram.net/air-conditioning.html"
       },{
         "title": "How to kill your grass",
         "excerpt":"I hate lawns. Okay, that’s not entirely true.Turfgrass excels at a few things and I do appreciate its utility in situations where it is called for.A significant section of our yard remains turf and we plan to leave it that way.However, we’ve killed a lot of grass since moving in...","categories": [],
         "tags": ["Garden","How to"],
-        "url": "https://house.youngram.com/how-to-kill-your-grass.html"
+        "url": "https://house.youngram.net/how-to-kill-your-grass.html"
       },{
         "title": "Garden Bridges",
         "excerpt":"As part of the rain garden, I had dug two channels to carry water from the downspouts to the basin.The infrequently used path on that side of the house crosses those two channels.Once I got them fixed up nice with aluminum edging, I wanted to do something at the crossings...","categories": [],
         "tags": [],
-        "url": "https://house.youngram.com/garden-bridges.html"
+        "url": "https://house.youngram.net/garden-bridges.html"
+      },{
+        "title": "A new home on the web (don't worry, we're keeping our physical home)",
+        "excerpt":"Just a quick note for the dedicated readers: this blog will now be hosted at house.youngram.net.It was previously hosted at house.youngram.com, so if you have it bookmarked, update your bookmarks.Someone contacted me with an offer to sell the youngram.com domain and offered enough money to make it worth my while,...","categories": [],
+        "tags": [],
+        "url": "https://house.youngram.net/a-new-home-on-the-web-don-t-worry-we-re-keeping-our-physical-home.html"
       }]
